5d453582fe87fda26dd3f9544cc81ef92b7a467e
NguyenTien2401 <hoangtienflytom@gmail.com>
2022-02-15 07:11:58 +0700

    update 7

--- src/services/connectDB.js
+++ src/services/connectDB.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2/promise');
 
 // create the connection to database
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'data|_adx',
-  // password: 'hoangtien14'
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b6db61ab5f5cf6',
+  database: 'heroku_e1f1835a99e4751',
+  password: 'd0769e5a'
 });
 
 module.exports = connection;
 
-// mysql://b981b5d5dd160e:8fb03229@us-cdbr-east-05.cleardb.net/heroku_a5d00162d031312?reconnect=true
+// mysql://b6db61ab5f5cf6:d0769e5a@us-cdbr-east-05.cleardb.net/heroku_e1f1835a99e4751?reconnect=true
